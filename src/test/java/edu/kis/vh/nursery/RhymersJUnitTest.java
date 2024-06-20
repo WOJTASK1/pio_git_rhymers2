e0a02a89a8c12727b057997a9c7e575b905e6960
WOJTASK1 <wojtek03.lep@gmail.com>
2024-06-20 21:55:56 +0200

    15.walidacja projektu testami jednostkowymi-pomyślna

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class RhymersJUnitTest {
+public class RhymersJUnitTest { //tested!
 
     @Test
     public void testCountIn() {
